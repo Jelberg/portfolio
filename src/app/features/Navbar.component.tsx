4932d2d416afb731bbc0876356defae450861c32
Jessica Elberg <elbergjessica@gmail.com>
2024-07-05 23:10:39 -0400

    Add style component and the firts component: header

---
+++ src/app/features/Navbar.component.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Avatar, NavbarMenuToggle, NavbarMenu, NavbarMenuItem} from "@nextui-org/react";
+import {VerifiedIcon} from "@/app/assets/VerifiedIcon";
+
+
+export default function NavbarComponent() {
+
+  const menuItems = [
+    "Experiencia",
+    "Proyectos",
+    "Contáctame",
+  ];
+
+  return (
+    <Navbar disableAnimation isBordered>
+      
+
+      <NavbarBrand className="gap-4">
+      <Avatar src={'img/photo.jpg'} size="md" className="rounded-full" />
+        <p className="font-bold text-inherit">Jessica Elberg</p>
+        <VerifiedIcon className="bg-white" color="#1379F3" />
+      </NavbarBrand>
+
+      <NavbarContent className="hidden sm:flex gap-4" justify="center">
+        <NavbarItem>
+          <Link className="font-semibold" color="foreground" href="#">
+            Experiencia
+          </Link>
+        </NavbarItem>
+        <NavbarItem >
+          <Link className="font-semibold" href="#" color="foreground">
+            Proyectos
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link className="font-semibold" color="foreground" href="#">
+            Contáctame
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+
+      <NavbarContent className="sm:hidden" justify="end">
+        <NavbarMenuToggle />
+      </NavbarContent>
+
+      <NavbarMenu>
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link
+              className="w-full"
+              color={
+                index === 2 ? "warning" : index === menuItems.length - 1 ? "danger" : "foreground"
+              }
+              href="#"
+              size="lg"
+            >
+              {item}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+
+    </Navbar>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,45 @@
+// tailwind.config.js
+import {nextui} from "@nextui-org/react";
+
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      }
+    },
+  },
+  darkMode: "class",
+  plugins: [
+      nextui({
+      prefix: "nextui", // prefix for themes variables
+      addCommonColors: false, // override common colors (e.g. "blue", "green", "pink").
+      defaultTheme: "dark", // default theme from the themes object
+      defaultExtendTheme: "dark", // default theme to extend on custom themes
+      layout: {}, // common layout tokens (applied to all themes)
+      themes: {
+        light: {
+          layout: {}, // light theme layout tokens
+          colors: {}, // light theme colors
+        },
+        dark: {
+          layout: {
+          }, // dark theme layout tokens
+          colors: {
+            primary: "#1379F3",
+          }, // dark theme colors
+        },
+        // ... custom themes
+      },
+    }),
+  ]
+}
+
+export default config;
